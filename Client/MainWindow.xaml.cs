008ca3cfd04ba454e1c4d8ab01a613dee7a80c67
Tural <Tural@TURAL>
2023-03-25 00:36:39 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Client
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 45678;
+            remoteEP = new IPEndPoint(ip, port);
+        }
+
+        private Socket client;
+        private EndPoint remoteEP;
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var buffer = new byte[ushort.MaxValue - 29];
+            await client.SendToAsync(buffer, SocketFlags.None, remoteEP);
+            var list = new List<byte>();
+            var len = 0;
+            do
+            {
+                SocketReceiveFromResult result = default;
+                try
+                {
+                    result = await client.ReceiveFromAsync(buffer, SocketFlags.None, remoteEP);
+                    len = result.ReceivedBytes;
+
+                    list.AddRange(buffer.Take(len));
+
+                    try
+                    {
+                        BitmapImage image;
+                        var imageData = list.ToArray();
+
+                        if (imageData == null || imageData.Length == 0)
+                            image = null;
+                        else
+                        {
+                            image = new BitmapImage();
+                            using (var mem = new MemoryStream(imageData))
+                            {
+                                mem.Position = 0;
+                                image.BeginInit();
+                                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                                image.CacheOption = BitmapCacheOption.OnLoad;
+                                image.UriSource = null;
+                                image.StreamSource = mem;
+                                image.EndInit();
+                            }
+                            image.Freeze();
+                            
+                        }
+
+                        if (image != null)
+                            imageView.Source = image;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Server is not running");
+                }
+
+            } while (len == buffer.Length);
+
+        }
+
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+
+var listener = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 45678;
+
+var buffer = new byte[ushort.MaxValue - 29];
+var ep = new IPEndPoint(ip, port);
+listener.Bind(ep);
+
+EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+
+
+while (true)
+{
+    var result = await listener.ReceiveFromAsync(buffer, SocketFlags.None, remoteEp);
+
+
+    Bitmap memoryImage;
+    var width = 1366;
+    var height = 768;
+
+    memoryImage = new Bitmap(width, height);
+
+    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+    memoryGraphics.CopyFromScreen(0, 0, 0, 0, memoryImage.Size);
+
+    var img = (Image)memoryImage;
+    byte[] imgBuffer;
+
+    using (var stream = new MemoryStream())
+    {
+        img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+        imgBuffer = stream.ToArray();
+    }
+
+
+    var chunk = imgBuffer.Chunk(ushort.MaxValue - 29);
+    var newBuffer = chunk.ToArray();
+
+    for (int i = 0; i < newBuffer.Length; i++)
+    {
+        await Task.Delay(50);
+        await listener.SendToAsync(newBuffer[i], SocketFlags.None, result.RemoteEndPoint);
+    }
+}
